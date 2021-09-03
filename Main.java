410504235d45d3f7f83ae1dfeb7743fd18211c90
Steven <steven.poulin.2@ulaval.ca>
2021-09-03 11:05:28 -0400

    Numero 1 du Lab 1
    Pour la deuxieme partie, un if ou switch avec une fonction .equals?

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+import java.lang.*;
+/* Programme Bonjour des notes transformé pour le premier numero. Je ne prends pas controle mais bien juste commmit le premier, pour le fun. On peut le changer */
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Helloddddd World");
-  }
+    
+    // Le main prends des arguments et les imprime un par ligne
+    public static void main(String args[]) {
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
 }
-// some changes
 // commentaire de Thierry
 // hourra
+// Steven: yes! java, qui l'eut cru
