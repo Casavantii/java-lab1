b5977938af33b2a655020219f29898814f6482db
Steven <steven.poulin.2@ulaval.ca>
2021-09-09 15:44:45 -0400

    Create SplitChaine_SP.java

---
+++ SplitChaine_SP.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+/* Programme Bonjour des notes transformé pour le premier numero. Je ne prends pas controle mais bien juste commmit le premier, pour le fun. On peut le changer */
+public class SplitChaine {
+    
+    // Le main prends une chaine, crée des Strings est les ajoutes a un vecteur (dynamique) et imprime ensuite les elements du vecteur un par ligne
+    public static void main(String args[]) {
+      
+      String initString = args[0];
+      
+      Vector strings = new Vector();
+      String base = "";
+      
+      for (int i = 0; i < initString.length(); i++){
+          if (initString.charAt(i) == ' ') {
+              strings.addElement(base);
+              base = "";
+          }
+          else if (i == initString.length() - 1) {
+              base += initString.charAt(i); 
+              strings.addElement(base);
+          }
+          else {
+              base += initString.charAt(i);
+          }
+          
+          
+          
+      }
+      
+      for (int i = 0; i < strings.size(); i++) {
+          System.out.println(strings.get(i));
+      }
+        
+    }
+}
