68f73c27e8f90bf886ed2613d75d493084c15b2d
Steven <steven.poulin.2@ulaval.ca>
2021-09-09 16:11:12 -0400

    Update ChangeSousChaine_SP.java

--- ChangeSousChaine_SP.java
+++ ChangeSousChaine_SP.java
@@ -1,36 +1,53 @@
 import java.lang.*;
 import java.util.*;
 /* Programme Bonjour des notes transformé pour le premier numero. Je ne prends pas controle mais bien juste commmit le premier, pour le fun. On peut le changer */
-public class SplitChaine {
+public class ChangeSousChaine {
     
     // Le main prends une chaine, crée des Strings est les ajoutes a un vecteur (dynamique) et imprime ensuite les elements du vecteur un par ligne
     public static void main(String args[]) {
+      if (args.length == 3) {
+        String initString = args[0];
+        String searchTerm = args[1];
+        String newTerm = args[2];
       
-      String initString = args[0];
       
-      Vector strings = new Vector();
-      String base = "";
       
-      for (int i = 0; i < initString.length(); i++){
-          if (initString.charAt(i) == ' ') {
-              strings.addElement(base);
-              base = "";
+      
+          Vector strings = new Vector();
+          String base = "";
+          
+          for (int i = 0; i < initString.length(); i++){
+              if (initString.charAt(i) == ' ') {
+                  strings.addElement(base);
+                  base = "";
+              }
+              else if (i == initString.length() - 1) {
+                  base += initString.charAt(i); 
+                  strings.addElement(base);
+              }
+              else {
+                  base += initString.charAt(i);
+              }
           }
-          else if (i == initString.length() - 1) {
-              base += initString.charAt(i); 
-              strings.addElement(base);
+          
+          int indexOfSearchTerm = strings.indexOf(searchTerm);
+          
+          if (indexOfSearchTerm == -1) {
+              System.out.println("Ne contient pas ce terme");
           }
           else {
-              base += initString.charAt(i);
+              strings.set(indexOfSearchTerm, newTerm) ;
+                
           }
           
-          
-          
-      }
-      
-      for (int i = 0; i < strings.size(); i++) {
-          System.out.println(strings.get(i));
-      }
-        
+          for (int i = 0; i < strings.size(); i++) {
+              System.out.print(strings.get(i) + " ");
+          }
+            
+        }
+    
+    else {
+        System.out.println("Pas assez ou trop d'elements. 3 elements necessaires");
+    }
     }
 }
